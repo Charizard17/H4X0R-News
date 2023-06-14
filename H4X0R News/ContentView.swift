6486cfb140b24ab939cc620e6287cd2c2f002798
Charizard17 <3723esad@gmail.com>
2023-06-14 21:50:02 +0200

    initial commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Esad Dursun on 14.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
